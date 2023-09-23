f6e0f5ca2980fb772009183deb67ffd63920f5ab
suryakant thombare <suryakant.thombare@ispace.com>
2023-09-24 00:00:26 +0530

    NearbyJobCard added

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
+// const rapidApiKey = NODE_ENV['RAPID_API_KEY'];
 
-import styles from './nearbyjobs.style'
+import useFetch from "../../../hook/useFetch";
+import styles from "./nearbyjobs.style";
+import { COLORS } from "../../../constants";
+import NearbyJobCard from "../../common//cards/nearby/NearbyJobCard";
 
-const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
+const NearbyJobs = () => {
+  const router = new useRouter();
+
+  const {isLoading, data, error} = useFetch(
+    'search', {
+      query: 'React develper',
+      num_pages: 1
+    }
   )
-}
 
-export default Nearbyjobs
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>NearBy jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator
+            style={styles.loadingIndicator}
+            size="large"
+            colors={COLORS.primary}
+          />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard 
+              job={job}
+              key={`nearby_job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/jobs-details/${job?.job_id}`)}
+            />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default NearbyJobs;
